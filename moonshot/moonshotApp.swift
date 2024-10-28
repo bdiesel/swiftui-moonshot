045150a165b11456173d9b6db4573d43292ad2f0
Brian Diesel <bri@ndiesel.net>
2024-10-28 10:18:06 -0400

    xcode scaffold

---
+++ moonshot/moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotApp.swift
+//  moonshot
+//
+//  Created by Brian Diesel on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moonshotTests/moonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotTests.swift
+//  moonshotTests
+//
+//  Created by Brian Diesel on 10/28/24.
+//
+
+import Testing
+@testable import moonshot
+
+struct moonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
