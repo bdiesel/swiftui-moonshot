ddf0664b393127a7b3e893c9c0161862140c2b43
Brian Diesel <bri@ndiesel.net>
2024-10-29 12:19:53 -0400

    Load data into grid

---
+++ moonshot/Color-Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Color-Theme.swift
+//  moonshot
+//
+//  Created by Brian Diesel on 10/29/24.
+//
+
+import Foundation
+import SwiftUICore
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
